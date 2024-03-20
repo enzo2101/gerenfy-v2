65193a29ddc5f48ad577a9a6923c88d735c6e9b1
Enzo Ragazzoni <enzoragazzoni21@gmail.com>
2024-03-20 17:04:19 -0300

    create form with zod, hook form and hookform resolver

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,80 +2,173 @@
 
 import Link from "next/link";
 
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { IconContext } from "react-icons";
 
 import { FaFacebook } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { SiGmail } from "react-icons/si";
 
+const userLoginSchema = z.object({
+  email: z
+    .string()
+    .nonempty("O email é obrigatório")
+    .email("Formato de email inválido")
+    .toLowerCase()
+    .refine(email => {
+      return email.endsWith("@gmail.com")
+    }, "O email precisa ser do Gmail")
+    .transform(email => {
+      return email.trim()
+    }),
+  password: z
+    .string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres")
+    .nonempty("A sennha é obrigatório"),
+  // isRemember: z.boolean(),
+});
+
+type CreateUserFormat = z.infer<typeof userLoginSchema>;
+
 const login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormat >({
+    resolver: zodResolver(userLoginSchema),
+  });
+
+  const [output, setOutput] = useState("");
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
   return (
-    <main className="bg-zinc-800 min-h-screen flex justify-center items-center">
-
-      <div className="h-screen items-center p-6 flex lg:space-x-60">
-
+    <main className="flex min-h-screen items-center justify-center bg-zinc-800">
+      <div className="flex h-screen items-center p-6 lg:space-x-60">
         <div className="max-w-md">
-
           <div className="space-y-5 text-center">
-            <h1 className="text-5xl text-white font-bold">Login</h1>
-            <p className="text-white lg:text-xl">Se você não tiver uma conta, você pode se <Link className="text-blue-500 font-bold" href="/register">registrar aqui!</Link></p>
+            <h1 className="text-5xl font-bold text-white">Login</h1>
+            <p className="text-white lg:text-xl">
+              Se você não tiver uma conta, você pode se{" "}
+              <Link className="font-bold text-blue-500" href="/register">
+                registrar aqui!
+              </Link>
+            </p>
           </div>
 
-          <div className="pt-12">
-            <div className="flex flex-col space-y-6">
-              <div className="relative z-0 w-full mb-5 group">
-                <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-300 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Insira o seu Email</label>
-              </div>
-              <div className="flex flex-col space-y-4">
-                <div className="relative z-0 w-full mb-5 group">
-                  <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                  <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-300 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Insira sua Senha</label>
+          <form onSubmit={handleSubmit(createUser)}>
+            <div className="pt-12">
+              <div className="flex flex-col space-y-6">
+                <div className="group relative z-0 mb-5 w-full">
+                  <input
+                    type="email"
+                    id="floating_email"
+                    className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-300 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                    placeholder=" "
+                    {...register("email")}
+                    required
+                  />
+                  <label
+                    htmlFor="floating_email"
+                    className="transhtmlForm absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+                  >
+                    Insira o seu Email
+                  </label>
+                  {errors.email && <span className="text-gray-300">{errors.email.message}</span>}
                 </div>
-                <div className="flex items-center mb-4">
-                  <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-transparent border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                  <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-300 dark:text-gray-300">Lembrar senha</label>
+                <div className="flex flex-col space-y-4">
+                  <div className="group relative z-0 mb-5 w-full">
+                    <input
+                      type="password"
+                      id="floating_password"
+                      className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-300 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                      placeholder=" "
+                      {...register("password")}
+                      required
+                    />
+                    <label
+                      htmlFor="floating_password"
+                      className="transhtmlForm absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500 rtl:peer-focus:translate-x-1/4"
+                    >
+                      Insira sua Senha
+                    </label>
+                    {errors.password && <span className="text-gray-300">{errors.password.message}</span>}
+                  </div>
+                  {/* <div className="mb-4 flex items-center">
+                    <input
+                      id="default-checkbox"
+                      type="checkbox"
+                      value=""
+                      className="h-4 w-4 rounded border-gray-300 bg-transparent text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                      {...register("isRemeber")}
+                    />
+                    <label
+                      htmlFor="default-checkbox"
+                      className="ms-2 text-sm font-medium text-gray-300 dark:text-gray-300"
+                    >
+                      Lembrar senha
+                    </label>
+                  </div> */}
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="relative z-0 w-full mb-5 group pt-14">
-            <button type="submit" className="text-gray-300 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-2xl text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Entrar</button>
-          </div>
+            <div className="group relative z-0 mb-5 w-full pt-14">
+              <button
+                type="submit"
+                className="w-full rounded-2xl bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-gray-300 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Entrar
+              </button>
+            </div>
+            <pre>{output}</pre>
+          </form>
 
-          <div className="flex flex-col justify-center items-center space-y-6">
+          <div className="flex flex-col items-center justify-center space-y-6">
             <p className="text-gray-300">Faça Login com</p>
             <div className="flex justify-center space-x-6">
               <Link href="/">
-                <IconContext.Provider value={{ className: "text-blue-500 text-3xl" }}>
+                <IconContext.Provider
+                  value={{ className: "text-blue-500 text-3xl" }}
+                >
                   <FaFacebook />
                 </IconContext.Provider>
               </Link>
               <Link href="/">
-                <IconContext.Provider value={{ className: "text-green-500 text-3xl" }}>
+                <IconContext.Provider
+                  value={{ className: "text-green-500 text-3xl" }}
+                >
                   <IoLogoWhatsapp />
                 </IconContext.Provider>
               </Link>
               <Link href="/">
-                <IconContext.Provider value={{ className: "text-red-500 text-3xl" }}>
+                <IconContext.Provider
+                  value={{ className: "text-red-500 text-3xl" }}
+                >
                   <SiGmail />
                 </IconContext.Provider>
               </Link>
             </div>
-
           </div>
-
         </div>
 
         <div className="collapse lg:visible">
-          <img className="rounded-2xl" src="./LoginPage.png" alt="Imagem Complementar página de Login" />
+          <img
+            className="rounded-2xl"
+            src="./LoginPage.png"
+            alt="Imagem Complementar página de Login"
+          />
         </div>
-
       </div>
-
-    </main >
+    </main>
   );
-}
+};
 
 export default login;
